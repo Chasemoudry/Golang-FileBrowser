ed3577986a4e9a0895a584719eb6b390e361b788
ChaseMoudry <chasemoudry@gmail.com>
2018-09-05 14:02:36 -0700

    Updated README and fixed subpathing bugs

--- README.md
+++ README.md
@@ -1,7 +1,15 @@
 # GoLang-FileBrowser
 ## App Structure
-
+Using Golang, the application listens for an HTTP request to be sent on port 8080. Once a valid request is received, the user is able to navigate throughout the server's file directory and download/view the files within.
 
 ## Tests
+### Directory Paging
+- Create a large number of files in a directory then navigate to that directory. Check to see if the paging list has no more than the paging limit and test if the cursory query is functional
+### Test for properly handled errors
+- Attempt to load a series of invalid pages to see if any result in an uncaught error (e.g., server not responding, server-side fatal logs).
 
 ## Missing Requirements
+- Should the sorted directory results show directory contents in an order that disregards the file type?
+- Should partial file downloading be handled where only a portion of the file is being previewed and not being permanently downloaded (Similar to how you can preview books through services such as Google Books or Amazon Kindle)? If not, what does an alternative to this system look like?
+- How should this application handle errors (e.g., HTTP errors, go fatals)?
+- Should paging be handled by queries?
--- src/github.com/chasemoudry/server/RequestHandler.go
+++ src/github.com/chasemoudry/server/RequestHandler.go
@@ -14,9 +14,10 @@ import (
 
 //File ...
 type File struct {
-	Path  string
-	Title string
-	Body  []byte
+	Path       string
+	Title      string
+	ParentPath string
+	Body       []byte
 }
 
 //Directory ...
@@ -43,10 +44,16 @@ var validPath = regexp.MustCompile("^\\.\\/files((\\/([0-9]+\\/)*[0-9]+\\?page=(
 
 func getSubPath(path string) string {
 	fullPath := strings.Split(path, "/")
-	if len(fullPath) == 0 {
+	pathLen := len(fullPath)
+	if pathLen == 0 {
 		return ""
 	}
-	return fullPath[(len(fullPath) - 1)]
+	if fullPath[(pathLen-1)] == "" {
+		fmt.Printf("DEBUG LOG: subpath for %s = %s\n", path, fullPath[(pathLen-2)])
+		return fullPath[(pathLen - 2)]
+	}
+	fmt.Printf("DEBUG LOG: subpath for %s = %s\n", path, fullPath[(pathLen-1)])
+	return fullPath[(pathLen - 1)]
 }
 
 func urlFromPath(path string) string {
@@ -65,31 +72,26 @@ func getParentPath(path string) string {
 	return strings.TrimSuffix(path, getSubPath(path))
 }
 
-func fileToString(startPath string, files []os.FileInfo) []DirectoryInfo {
+func getDirectoryInfo(path string, files []os.FileInfo) []DirectoryInfo {
 	dirInfo := make([]DirectoryInfo, len(files), len(files))
 	for i, v := range files {
-		if startPath == rootDir {
-			dirInfo[i].FullPath = startPath + v.Name()
+		if path == rootDir {
+			dirInfo[i].FullPath = path + v.Name()
 		} else {
-			dirInfo[i].FullPath = startPath + "/" + v.Name()
+			dirInfo[i].FullPath = path + "/" + v.Name()
 		}
 		dirInfo[i].SubPath = v.Name()
 	}
 	return dirInfo
 }
 
-func loadFile(path string) (*File, error) {
-	body, err := ioutil.ReadFile(path)
-	if err != nil {
-		return nil, err
-	}
-	return &File{Path: urlFromPath(path), Title: getSubPath(path), Body: body}, nil
-}
-
 func getPagingSlice(targetDir []os.FileInfo, pageNum int) []os.FileInfo {
 	sliceLen := len(targetDir)
-	fmt.Printf("DEBUG LOG: SLICING FROM %d:%d\n", (pageNum-1)*pagingLimit, sliceLen-(pageNum*pagingLimit))
-	targetDir = targetDir[(pageNum-1)*pagingLimit : sliceLen-(pageNum*pagingLimit)]
+	if pageNum*pagingLimit <= sliceLen {
+		targetDir = targetDir[(pageNum-1)*pagingLimit : (pageNum * pagingLimit)]
+	} else {
+		targetDir = targetDir[(pageNum-1)*pagingLimit : sliceLen]
+	}
 	return targetDir
 }
 
@@ -115,23 +117,18 @@ func renderDirectory(w http.ResponseWriter, tmpl string, dir *Directory) {
 	}
 }
 
-func renderFile(w http.ResponseWriter, tmpl string, f *File) {
-	fmt.Println("DEBUG LOG: File template loaded.")
-	err := pageTemplates.ExecuteTemplate(w, tmpl+".html", f)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
-}
-
 func directoryHandler(w http.ResponseWriter, r *http.Request, filePath string) {
 	fmt.Println("DEBUG LOG: Directory '" + filePath + "' loaded.")
 	if r.URL.RawQuery == "" {
 		if filePath == rootDir {
-			fmt.Println("DEBUG?WRN: DIRECTORY REDIRECTING to " + "?page=1")
+			filePath = strings.TrimSuffix(filePath, "/")
+			fmt.Println("DEBUG?WRN: DIRECTORY REDIRECTING to ?page=1")
 			http.Redirect(w, r, "?page=1", http.StatusFound)
 		} else {
+			filePath = strings.TrimSuffix(filePath, "/")
 			fmt.Println("DEBUG?WRN: DIRECTORY REDIRECTING to " + filePath + "?page=1")
-			http.Redirect(w, r, getSubPath(filePath)+"?page=1", http.StatusFound)
+			// http.Redirect(w, r, getSubPath(filePath)+"?page=1", http.StatusFound)
+			http.Redirect(w, r, "?page=1", http.StatusFound)
 		}
 		return
 	}
@@ -140,19 +137,41 @@ func directoryHandler(w http.ResponseWriter, r *http.Request, filePath string) {
 	if readErr != nil {
 		fmt.Println("DEBUG!ERR: " + readErr.Error())
 		renderFile(w, "file", &File{
-			Path:  urlFromPath(filePath),
-			Title: "Error",
-			Body:  []byte(readErr.Error())})
+			Path:       urlFromPath(filePath),
+			Title:      "Error",
+			ParentPath: getParentPath(filePath),
+			Body:       []byte(readErr.Error())})
 	}
 	files = getPagingSlice(files, currentPage)
 	// for _, file := range files {
 	// 	fmt.Println("DEBUG LOG: " + file.Name())
 	// }
 	renderDirectory(w, "directory", &Directory{
-		Path:       filePath,
-		Title:      urlFromPath(filePath),
+		Path:       urlFromPath(filePath),
+		Title:      getSubPath(filePath),
 		ParentPath: getParentPath(filePath),
-		Dir:        fileToString(filePath, files)})
+		Dir:        getDirectoryInfo(filePath, files)})
+}
+
+func renderFile(w http.ResponseWriter, tmpl string, f *File) {
+	fmt.Println("DEBUG LOG: File template loaded.")
+	err := pageTemplates.ExecuteTemplate(w, tmpl+".html", f)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func loadFile(filePath string) (*File, error) {
+	body, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		return nil, err
+	}
+	return &File{
+			Path:       urlFromPath(filePath),
+			Title:      getSubPath(filePath),
+			ParentPath: getParentPath(filePath),
+			Body:       body},
+		nil
 }
 
 func fileHandler(w http.ResponseWriter, r *http.Request, filePath string) {
@@ -162,9 +181,10 @@ func fileHandler(w http.ResponseWriter, r *http.Request, filePath string) {
 		if strings.Contains(loadErr.Error(), "The system cannot find the path specified.") {
 			fmt.Println("DEBUG!ERR: " + loadErr.Error())
 			renderFile(w, "file", &File{
-				Path:  urlFromPath(filePath),
-				Title: "Error",
-				Body:  []byte(loadErr.Error())})
+				Path:       urlFromPath(filePath),
+				Title:      "Error",
+				ParentPath: getParentPath(filePath),
+				Body:       []byte(loadErr.Error())})
 			return
 		}
 	}
@@ -185,9 +205,10 @@ func pageHandler(w http.ResponseWriter, r *http.Request) {
 		if os.IsNotExist(statErr) {
 			fmt.Println("DEBUG!ERR: " + statErr.Error())
 			renderFile(w, "file", &File{
-				Path:  r.URL.Path,
-				Title: "Error",
-				Body:  []byte(statErr.Error())})
+				Path:       r.URL.Path,
+				Title:      "Error",
+				ParentPath: getParentPath(filePath),
+				Body:       []byte(statErr.Error())})
 			return
 		}
 	}
@@ -199,7 +220,7 @@ func pageHandler(w http.ResponseWriter, r *http.Request) {
 		// When path leads to file
 		fileHandler(w, r, filePath)
 	default:
-		fmt.Println("DEBUG?WRN: " + filePath + " mode = " + strconv.Itoa((int)(mode)))
+		fmt.Println("DEBUG?WRN: " + filePath + " unrecognized mode = " + strconv.Itoa((int)(mode)))
 	}
 }
 
@@ -209,14 +230,6 @@ func redirectToRoot(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	// files, err := ioutil.ReadDir("./files/001/")
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// for _, file := range files {
-	// 	fmt.Println("DEBUG LOG: " + file.Name())
-	// }
-
 	http.HandleFunc("/", redirectToRoot)
 	http.HandleFunc("/files/", pageHandler)
 
